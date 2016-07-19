7d6684f429926d54dc3a5dea7e824683bb70575a
Enrico Rubboli <rubboli@gmail.com>
2016-07-19 12:53:10 +0200

    use base http that follow configuration

--- lib/grenache/base.rb
+++ lib/grenache/base.rb
@@ -1,22 +1,26 @@
 module Grenache
   class Base
     def listen(key, port,  opts={}, &block)
-      EM.defer {
-        app = -> (env) {
-          block.call(env)
-        }
-        server = Thin::Server.start('0.0.0.0', port, app, {signals: false})
-      }
+      start_http_service(port,&block)
 
       announce(key, port, opts) do |res|
         puts "#{key} announced #{res}"
       end
     end
 
+    def start_http_service(port, &block)
+      EM.defer {
+        app = -> (env) {
+          block.call(env)
+        }
+        server = Thin::Server.start('0.0.0.0', port, app, {signals: false})
+      }
+    end
+
     def request(key, payload, &block)
       services = lookup(key)
       json = Oj.dump(payload)
-      res = HTTPClient.new.post(services.sample,json).body
+      res = http.post(services.sample,json).body
     end
   end
 end
