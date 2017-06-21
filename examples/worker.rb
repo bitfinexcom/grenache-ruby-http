640e18e7121af2ffc8665ab5cc4f491b933d493c
Enrico Rubboli <rubboli@gmail.com>
2017-06-21 13:13:37 +0200

    bump base version

--- examples/worker.rb
+++ examples/worker.rb
@@ -1,15 +1,17 @@
 require_relative '../lib/grenache-ruby-http.rb'
 
-Grenache::Http.configure do |conf|
-   conf.grape_address = "http://127.0.0.1:40002/"
-end
+# This is another way to set a configuration:
+#
+# Grenache::Http.configure do |conf|
+#   conf.grape_address = "http://127.0.0.1:40002/"
+# end
 
 EM.run do
 
   Signal.trap("INT")  { EventMachine.stop }
   Signal.trap("TERM") { EventMachine.stop }
 
-  c = Grenache::Http.new
+  c = Grenache::Http.new grape_address: "http://127.0.0.1:40002/"
 
   c.listen('rpc_test', 5004) do |msg|
     #[StandardError.new("Error!"),"hello #{msg.payload}"]
--- lib/grenache/http/version.rb
+++ lib/grenache/http/version.rb
@@ -1,5 +1,5 @@
 module Grenache
   module HTTP
-    VERSION = "0.2.8"
+    VERSION = "0.2.9"
   end
 end
